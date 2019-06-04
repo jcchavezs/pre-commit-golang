5766ebe4a4a62302c83c01b47d41d5a6dae18017
José Carlos Chávez <jcchavezs@gmail.com>
2019-06-04 14:16:43 +0200

    fix: fixes golangci multiple files.

--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 set -e -u -o pipefail # Fail on error
 
-golangci-lint run $@
+args=""
+fns=()
+
+# golangci does not allow to pass more than one file if they don't belong
+# to the same directory (see https://github.com/golangci/golangci-lint/issues/391)
+# thus we need to run the command file by file.
+
+for arg in "$@"; do
+	if [[ $arg == --* ]]; then
+		args="$args $arg"
+  else
+    fns+=("$arg")
+	fi
+done
+
+for file in ${fns[*]}
+do
+  golangci-lint run $args $file
+done
