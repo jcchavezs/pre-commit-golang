074e49cbf38d76d2d2e0632c18de1fd4e22db8b2
José Carlos Chávez <jcchavezs@gmail.com>
2019-06-11 14:24:52 +0200

    chore: avoids ignored folders.

--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -7,12 +7,18 @@ fns=()
 # golangci does not allow to pass more than one file if they don't belong
 # to the same directory (see https://github.com/golangci/golangci-lint/issues/391)
 # thus we need to run the command file by file.
+#
+# Also from https://github.com/golangci/golangci-lint/blob/master/README.md#config-file:
+# > next dirs are always skipped independently from this option's value:
+#   vendor$, third_party$, testdata$, examples$, Godeps$, builtin$
 
 for arg in "$@"; do
 	if [[ $arg == --* ]]; then
 		args="$args $arg"
   else
-    fns+=("$arg")
+    if [[ ! $arg =~ [^(vendor|third_party|testdata|examples|Godeps|builtin)] ]]; then
+      fns+=("$arg")
+    fi
 	fi
 done
 
